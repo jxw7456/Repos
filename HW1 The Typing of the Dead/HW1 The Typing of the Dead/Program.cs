d71dffc050f5452dc3a57b733b8ae7853412a474
jxw7456 <jxw7456@GDD-09>
2016-09-11 16:10:56 -0400

    Methods Working

--- HW1 The Typing of the Dead/HW1 The Typing of the Dead/Program.cs
+++ HW1 The Typing of the Dead/HW1 The Typing of the Dead/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 //Name: JaJuan Webster
 //Code: HW1 The Typing of the Dead
 //Professor Maier
@@ -13,7 +14,18 @@ namespace HW1_The_Typing_of_the_Dead
     {
         static void Main(string[] args)
         {
+            ZombieData test = new ZombieData();
+            test.LoadPhrases("phrases.txt");
+            test.LoadZombies();
 
+            for (int i = 0; i < 30; i++)
+            {
+                Console.WriteLine(test.RandomPhrase());
+                Console.WriteLine(test.RandomZombie());
+            }
+            
+            //DEBUG
+            Console.ReadLine();
         }
     }
 }
--- HW1 The Typing of the Dead/HW1 The Typing of the Dead/ZombieData.cs
+++ HW1 The Typing of the Dead/HW1 The Typing of the Dead/ZombieData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 //Name: JaJuan Webster
 //Code: HW1 The Typing of the Dead
 //Professor Maier
@@ -24,13 +25,52 @@ namespace HW1_The_Typing_of_the_Dead
         //opens the file with this filename (as text), and reads all phrases, storing them in the phrases attritbute
         public void LoadPhrases(string filename)
         {
-
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader srPhrases = new StreamReader("phrases.txt"))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    string lineP;
+                    while ((lineP = srPhrases.ReadLine()) != null)
+                    {
+                        Console.WriteLine(lineP);
+                        phrases.Add(lineP);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("You are all out of phrases!");
+                Console.WriteLine(e.Message);
+            }
         }
 
         //read zombies in files zombie1.txt, etc for an arbitrary number of zombies
         public void LoadZombies()
         {
+            try
+            {
+                string[] zombFiles = Directory.GetFiles("./");
 
+                foreach (var file in zombFiles)
+                {
+                    if (file.Contains("zombie"))
+                    {
+                        // Open the text file using a stream reader.
+                        using (StreamReader srZombie = new StreamReader(file + ".txt"))
+                        {
+                            // Read the stream to a string, and write the string to the console.
+                            string lineZ = srZombie.ReadToEnd();
+                            Console.WriteLine(lineZ);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The are no more zombies left!");
+                Console.WriteLine(e.Message);
+            }
         }
 
         //returns a random phrase from the phrase list
