cf60aa4469782097b293bda892749d6e2c342ff4
jxw7456 <jxw7456@OH1370-17>
2016-09-12 16:48:12 -0400

    working

---
+++ EducationalBaloonTribe/EducationalBaloonTribe/BalloonExtra.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationalBaloonTribe
+{
+    //Allows multiple files (as many classes as you want)
+    partial class Balloon
+    {
+        public string Name { get; set; }
+
+
+    }
+}
