f983ac4a5665fe218032c0c2b21ea113261503c3
jxw7456 <jxw7456@OH1370-17>
2016-09-14 16:55:02 -0400

    Homework 2 Started

---
+++ HW2 First Monogame/HW2 First Monogame/GameObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+//Name: JaJuan Webster
+//HW2 First Monogame
+//Professor Maier
+
+namespace HW2_First_Monogame
+{
+    class GameObject
+    {
+        public struct Rectangle
+        {
+            int X { get; set; }
+            int Y { get; set; }
+        }
+          
+        public GameObject(int X, int Y, int width, int height, )
+        {
+                                
+        }
+    }
+}
