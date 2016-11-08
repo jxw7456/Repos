6fda470243787a7442f3f0a0d0174dde6dd232c9
jxw7456 <jxw7456@GDD-10>
2016-11-08 16:43:12 -0500

    PE21 Talent Tree Traversal COMPLETED

---
+++ PE21 Talent Tree Traversal/PE21 Talent Tree Traversal/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//JaJuan Webster
+//PE21 Talent Tree
+//Professor Maier
+
+namespace PE21_Talent_Tree_Traversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nodes: Star Wars
+            TalentTreeNode push = new TalentTreeNode("Force Push", true);
+            TalentTreeNode saberThrow = new TalentTreeNode("Force Saber Throw", true);
+            TalentTreeNode lightning = new TalentTreeNode("Force Lightning", true);
+            TalentTreeNode shield = new TalentTreeNode("Force Lightning Shield", false);
+            TalentTreeNode slash = new TalentTreeNode("Sith Slash", false);
+            TalentTreeNode repulse = new TalentTreeNode("Force Repulse", false);
+            TalentTreeNode sling = new TalentTreeNode("Sith Sling", false);
+
+            //Node Relationships
+            push.Left = saberThrow;
+            push.Right = lightning;
+            saberThrow.Left = shield;
+            saberThrow.Right = slash;
+            lightning.Left = repulse;
+            lightning.Right = sling;
+
+            //Known Abilities
+            Console.WriteLine("Known Abilities: ");
+            push.ListKnownAbilities(push);
+
+            //INDENT
+            Console.WriteLine();
+
+            //Possible Abilities
+            Console.WriteLine("Possible Abilities: ");
+            push.ListPossibleAbilities(push);
+
+            //DEBUG
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PE21 Talent Tree Traversal/PE21 Talent Tree Traversal/TalentTreeNode.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE21_Talent_Tree_Traversal
+{
+    class TalentTreeNode
+    {
+        //Attributes
+        private string abilityName;
+        Boolean hasLearned;
+        private TalentTreeNode left;
+        private TalentTreeNode right;
+
+        //Construtor
+        public TalentTreeNode(string name, bool learned)
+        {
+            abilityName = name;
+            hasLearned = learned;
+            left = null;
+            right = null;
+        }
+
+        //Properties
+        public TalentTreeNode Left
+        {
+            get { return left; }
+            set { left = value; }
+        }
+
+        public TalentTreeNode Right
+        {
+            get { return right; }
+            set { right = value; }
+        }
+
+        //Recursion
+        public void ListKnownAbilities(TalentTreeNode current)
+        {
+            if (current != null)
+            {
+                if (current.hasLearned == true)
+                {
+                    ListKnownAbilities(current.Left);
+                    Console.WriteLine(current.abilityName);
+                    ListKnownAbilities(current.Right);
+                }
+            }
+        }
+
+        public void ListPossibleAbilities(TalentTreeNode current)
+        {
+            if (current != null)
+            {
+                ListPossibleAbilities(current.Left);
+                if (current.hasLearned == false)
+                {
+
+                    Console.WriteLine(current.abilityName);
+
+                }
+                ListPossibleAbilities(current.Right);
+            }
+        }
+    }
+}
